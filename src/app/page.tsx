e32b10b31cd2c8afc08e9703a20d4ff237885e62
Md. Nayeem <renayeem@gmail.com>
2023-09-23 20:33:34 +0600

    emoji added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TrackCard from '@/components/shared/TrackCard';
 export default function Home() {
   return (
     <div className='pt-12'>
-      <p className='font-bold'>Popular Tracks</p>
+      <p className='font-bold'>Popular Tracks😍</p>
       <div className='grid grid-cols-2 gap-1.5 pt-4  md:grid-cols-3 md:gap-3 lg:grid-cols-4 2xl:grid-cols-5 2xl:gap-4'>
         <TrackCard />
       </div>
