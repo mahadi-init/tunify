6aa23917695e326ac042b6096d9643273cf4c090
Md. Nayeem <renayeem@gmail.com>
2023-10-07 21:35:23 +0600

    Loader and img tag changed
    - raw img tag reverted to the previous one because by using div tag the problem fixed... don't know why though
    
    - loading animation changed

--- src/app/pages/musics/[categories]/[id]/page.tsx
+++ src/app/pages/musics/[categories]/[id]/page.tsx
@@ -8,6 +8,7 @@ import { DB_COVER_IMAGES_LOCATION, DB_MUSIC_LOCATION } from '@/const/locations';
 import { useEffect, useState } from 'react';
 import MusicDb from '@/db/utils/db-music';
 import { getDownloadURL } from 'firebase/storage';
+import '../../../../../css/loader.css'
 
 export default function Music({
   params,
@@ -41,14 +42,29 @@ export default function Music({
 
   return (
     <div className='flex w-full flex-col items-center justify-center gap-4 pt-12'>
-      <Image
+
+      {
+        musicCover ? <div className='flex w-full flex-col items-center justify-center gap-4 pt-12'>
+          <Image
+            src={musicCover ?? '/gif/loading.gif'}
+            width={500}
+            height={500}
+            className='rounded-full object-cover'
+            alt='music cover'
+          />
+          <MusicControl downloadURL={downloadUrl} />
+        </div> : <svg className="loading-animation" preserveAspectRatio="xMidYMid meet" viewBox="0 0 187.3 93.7" height="300px" width="400px">
+          <path d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="4" fill="none" id="outline" stroke="#4E4FEB"></path>
+          <path d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="4" stroke="#4E4FEB" fill="none" opacity="0.05" id="outline-bg"></path>
+        </svg>
+      }
+      {/* <Image
         src={musicCover ?? '/gif/loading.gif'}
         width={500}
         height={500}
         className='rounded-full object-cover'
         alt='music cover'
-      />
-      <MusicControl downloadURL={downloadUrl} />
+      /> */}
     </div>
   );
 }
